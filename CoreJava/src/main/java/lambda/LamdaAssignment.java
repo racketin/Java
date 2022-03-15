feba19190c928e99d677ed7ea313ee5aad377ace
U-CORP\RachitChawla <RachitChawla@D9RJ81Z2.corp.fairisaac.com>
2022-03-15 20:26:33 +0530

    "More example"

--- CoreJava/src/main/java/lambda/LamdaAssignment.java
+++ CoreJava/src/main/java/lambda/LamdaAssignment.java
@@ -13,6 +13,17 @@ import java.util.function.Supplier;
  * 
  * Use Runnable if it does neither and cannot throw.
  * 
+ * 
+ * Supplier       ()    -> x
+Consumer       x     -> ()
+BiConsumer     x, y  -> ()
+Callable       ()    -> x throws ex
+Runnable       ()    -> ()
+Function       x     -> y
+BiFunction     x,y   -> z
+Predicate      x     -> boolean
+UnaryOperator  x1    -> x2
+BinaryOperator x1,x2 -> x3
  * @author RachitChawla
  *
  */
